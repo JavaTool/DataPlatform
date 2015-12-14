430b9db6247d9b9de20ae711d410dd9f635c9af9
hyfu <71806228>
2015-12-14 15:59:47 +0800

    a

---
+++ src/dataplatform/script/IScriptEngine.java
@@ -0,0 +1,11 @@
+package dataplatform.script;
+
+import java.io.File;
+
+public interface IScriptEngine {
+	
+	Object loadScript(File script) throws Exception;
+	
+	Object invokeFunction(String name, Object... args) throws Exception;
+
+}
